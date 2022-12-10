9e42dc5d19e269cb36ebb294341535d15aba875f
masxxiii <masxafar@gmail.com>
2022-12-10 22:21:27 +0700

    Initial Commit

---
+++ StarterKit/StarterKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarterKitApp.swift
+//  StarterKit
+//
+//  Created by Masood Zafar on 10.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct StarterKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
