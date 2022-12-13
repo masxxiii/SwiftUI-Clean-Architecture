94ff11f3fc24ad0f15d3669711f6489ae8a859f0
masxxiii <masxafar@gmail.com>
2022-12-13 23:00:24 +0700

    add API error and HTTP code Enums

---
+++ StarterKit/Core/Helpers/ErrorAPI.swift
@@ -0,0 +1,14 @@
+//
+//  APIErrors.swift
+//  StarterKit
+//
+//  Created by Masood Zafar on 13.12.2022.
+//
+
+import Foundation
+
+enum ErrorAPI: Error {
+    case BAD_INTERNET
+    case BAD_URL
+    case BAD_DECODING
+}
